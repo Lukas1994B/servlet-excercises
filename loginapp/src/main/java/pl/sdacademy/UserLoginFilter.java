46f03d013535152a33c30659d142d51f36304e2e
Łukasz Bresz <breszlukasz@gmail.com>
2019-02-02 12:09:51 +0100

    dodany filtr do logowania

---
+++ loginapp/src/main/java/pl/sdacademy/UserLoginFilter.java
@@ -0,0 +1,24 @@
+package pl.sdacademy;
+
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "UserLoginFiler", value = "/main")
+public class UserLoginFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request.getSession(false) == null){
+            response.sendRedirect("login.html");
+            return;
+        }
+        chain.doFilter(request,response);
+    }
+}
